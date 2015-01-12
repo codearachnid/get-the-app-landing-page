00329ae68c57b88a3c8fd1a4df1c7a86a0a7db8f
Timothy Wood <codearachnid@gmail.com>
2015-01-12 10:14:41 -0500

    Create get-the-app-landing-page.php

---
+++ get-the-app-landing-page.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: Get The App Landing Page
+ * Plugin URI:
+ * Description: Do you have an app for your WordPress site? Let your users know on landing. Compatible with Apple App Store & Android Play Store.
+ * Version: 0.0.1
+ * Author: Timothy Wood (@codearachnid)
+ * Author URI: http://www.codearachnid.com
+ * Author Email: tim@imaginesimplicity.com
+ * Text Domain: 'get-the-app-landing-page'
+ * License:
+ *
+ *     Copyright 2013 Imagine Simplicity (tim@imaginesimplicity.com)
+ *     License: GNU General Public License v3.0
+ *     License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @author codearachnid
+ *
+ */
+ 
+if ( ! defined( 'ABSPATH' ) ) die( '-1' );
